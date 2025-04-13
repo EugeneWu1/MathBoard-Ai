0a13212e9d69e4645d88e095aad58f62827cc8e6
EugeneWu1 <eugene.wu@unah.hn>
2025-04-13 15:14:09 -0600

    Se agregan comentarios al codigo

--- main.py
+++ main.py
@@ -1,45 +1,49 @@
-import tkinter as tk
-from PIL import Image, ImageDraw
-from recognizer import cargar_modelo, predecir_imagen
+import tkinter as tk #Biblioteca para crear interfaces en python
+from PIL import Image, ImageDraw #Para manipulacion de imagenes
+from recognizer import cargar_modelo, predecir_imagen #Modulo personalizado que contiene funciones para cargar un modelo y hacer predicciones
 
 
 class App:
     def __init__(self, root):
-        self.root = root
-        self.root.title("Canvas de Escritura")
-        self.canvas_width = 400
-        self.canvas_height = 200
+        self.root = root #root es la ventana principal
+        self.root.title("Canvas de Escritura") #Titulo de la ventana
+        self.canvas_width = 400  #Ancho
+        self.canvas_height = 200 #Alto
 
+        #Creacion de la pizarra con las dimensiones anteriores
         self.canvas = tk.Canvas(root, width=self.canvas_width, height=self.canvas_height, bg="white")
-        self.canvas.pack()
+        self.canvas.pack() #Lo coloca en la ventana principal
 
         # Imagen para guardar lo que se dibuja
-        self.image = Image.new("L", (self.canvas_width, self.canvas_height), "white")
-        self.draw = ImageDraw.Draw(self.image)
+        self.image = Image.new("L", (self.canvas_width, self.canvas_height), "white") 
+        self.draw = ImageDraw.Draw(self.image)  #Para poder dibujar sobre la imagen
 
-        self.canvas.bind("<B1-Motion>", self.paint)
+        self.canvas.bind("<B1-Motion>", self.paint)#Vincular el mov del raton con el click izquierdo
 
-        tk.Button(root, text="Guardar como imagen", command=self.guardar).pack(pady=10)
-        tk.Button(root, text="Limpiar", command=self.limpiar).pack()
+        tk.Button(root, text="Guardar como imagen", command=self.guardar).pack(pady=10)#Boton para guardar y predecir
+        tk.Button(root, text="Limpiar", command=self.limpiar).pack() #Boton para limpiar
 
+    #Se ejecuta cuando estamo presionando el click izquierdo
     def paint(self, event):
         x, y = event.x, event.y
         r = 8  # radio del trazo
-        self.canvas.create_oval(x - r, y - r, x + r, y + r, fill="black")
-        self.draw.ellipse([x - r, y - r, x + r, y + r], fill="black")
+        self.canvas.create_oval(x - r, y - r, x + r, y + r, fill="black") #Diubja un circulo negro en el canvas visible
+        self.draw.ellipse([x - r, y - r, x + r, y + r], fill="black") #Dibuja el mismo circulo en la imagen subyacente
 
     def guardar(self):
-        self.image.save("dibujo.png")
+        self.image.save("dibujo.png") #Guarda la imagen com dibuj
         print("Imagen guardada como dibujo.png")
-        modelo = cargar_modelo()
-        resultado = predecir_imagen("dibujo.png", modelo)
-        print("Predicción del modelo:", resultado)
+        modelo = cargar_modelo() #Cargar el modelo de reconocimiento (modulo_mnist.h5)
+        resultado = predecir_imagen("dibujo.png", modelo) #Pasa la imagen al modelo para obtener una prediccion
+        print("Predicción del modelo:", resultado) #Muestra el resultado en consola
 
+    #Borra todo el contenido en la pizarra
     def limpiar(self):
         self.canvas.delete("all")
         self.draw.rectangle([0, 0, self.canvas_width, self.canvas_height], fill="white")
 
+#Ejecucion principal
 if __name__ == "__main__":
-    root = tk.Tk()
-    app = App(root)
-    root.mainloop()
+    root = tk.Tk() #Crea la ventana principal
+    app = App(root) #Instancia de App
+    root.mainloop() #Inicio del bucle principal de la interfaz
--- recognizer.py
+++ recognizer.py
@@ -1,4 +1,4 @@
-import tensorflow as tf
+import tensorflow as tf #Biblioteca para crear y entrenar modelos de aprendizaje profundo
 from tensorflow.keras import layers, models
 from tensorflow.keras.datasets import mnist
 from tensorflow.keras.models import load_model
@@ -6,9 +6,11 @@ import numpy as np
 from PIL import Image
 
 # Cargar dataset de dígitos
+#mnist.load_data() carga el dataset MNIST
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
 # Normalizar imágenes
+#Divide los valores de píxeles (0-255) entre 255 para llevarlos a rango [0, 1]
 x_train = x_train / 255.0
 x_test = x_test / 255.0
 
@@ -18,29 +20,30 @@ x_test = x_test[..., None]
 
 # Modelo CNN simple
 model = models.Sequential([
-    layers.Conv2D(32, (3, 3), activation="relu", input_shape=(28, 28, 1)),
-    layers.MaxPooling2D(2, 2),
+    layers.Conv2D(32, (3, 3), activation="relu", input_shape=(28, 28, 1)), #28,28,1 son las dimensiones de las imagenes
+    layers.MaxPooling2D(2, 2), #Reduccion de dimensiones  2x2
     layers.Conv2D(64, (3, 3), activation="relu"),
     layers.MaxPooling2D(2, 2),
-    layers.Flatten(),
-    layers.Dense(64, activation="relu"),
-    layers.Dense(10, activation="softmax")  # Para 10 dígitos (0-9)
+    layers.Flatten(), #Aplana la salida para la capa densa
+    layers.Dense(64, activation="relu"), #Capa oculta con 64 neuronas
+    layers.Dense(10, activation="softmax")  # 10 neuronas para 10 dígitos (0-9)
 ])
 
 model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
 model.fit(x_train, y_train, epochs=5, validation_data=(x_test, y_test))
 
 # Guardar modelo
-model.save("model/cnn_digits.h5")
+model.save("model/modelo_digits.h5")
 
+#Simplemente carga y devuelve el modelo previamente guardado
 def cargar_modelo():
-    return load_model("model/cnn_digits.h5")
+    return load_model("model/modelo_digits.h5")
 
 def predecir_imagen(path_imagen, modelo):
-    img = Image.open(path_imagen).convert("L").resize((28, 28))
-    img = np.array(img)
+    img = Image.open(path_imagen).convert("L").resize((28, 28)) #L es escala de grises
+    img = np.array(img) 
     img = 255 - img  # invertir si es fondo blanco y número negro
-    img = img / 255.0
+    img = img / 255.0 #Escalar [0,1]
     img = img.reshape(1, 28, 28, 1)
     prediccion = modelo.predict(img)
     return np.argmax(prediccion)
@@ -72,4 +75,4 @@ if __name__ == "__main__":
     model.fit(x_train, y_train, epochs=5, validation_data=(x_test, y_test))
 
     # Guardar modelo
-    model.save("model/cnn_digits.h5")
+    model.save("model/modelo_digits.h5")
